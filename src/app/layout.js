8732781ea809bdf544abc18f53d76dcb9485c073
muskan <muskansuthar89@gmail.com>
2025-04-16 18:28:59 +0530

    add discription in meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,10 @@ import ThemeProvider from "@/context/ThemeProvider";
 
 export const metadata = {
   title: "ANGIRA ART EXPORTS",
+  description: "Established in 2005, Angira Art Exports is a globally recognized manufacturer and exporter of solid wood and iron furniture.",
   icons: {
     icon: "/logo.png",
-  },
+  }
 };
 
 export default function RootLayout({ children }) {
